23e8f455831babc4be9f762af596060cb91bc216
Deepank Agarwal <deepank411@gmail.com>
2022-06-04 18:38:15 +0530

    ga added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
 import MainSection from './components/main';
 import AboutSection from './components/about';
 import PassionSection from './components/passion';
 import ContactSection from './components/contact';
-
-import './styles/App.css';
 import FamilySection from './components/family';
 
+import './styles/App.css';
+
 function App() {
+	useEffect(() => {
+		ReactGA.initialize('UA-85185061-2');
+		if (window) {
+			ReactGA.pageview(window.location.pathname + window.location.search);
+		}
+	}, []);
 	return (
 		<div className="App">
 			<MainSection />
